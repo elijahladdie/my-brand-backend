1e26fab2edd55a54f8da7769b01b0b8c2cd14669
Elie Kuradusenge <68749686+elijahladdie@users.noreply.github.com>
2024-03-26 23:11:01 +0200

    ft-adding coverage test and actions (#17)

--- src/routes/blogRoutes.ts
+++ src/routes/blogRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { CreateBlog, GetBlog, createComment, deleteBlog, likeBlog, updateBlog } from '../controllers/blogController';
+import { CreateBlog, GetBlog, createComment, deleteAllBlogs, deleteBlog, likeBlog, updateBlog } from '../controllers/blogController';
 import upload from '../utility/multer';
 import isAuthenticated from '../utility/VerifyToken';
 import { CreateBlogValidationError, CreateCommentValidationError } from '../validations/BlogValidation';
@@ -7,13 +7,16 @@ import { CreateBlogValidationError, CreateCommentValidationError } from '../vali
 const router = express.Router();
 router.use(isAuthenticated)
 router.get('/all', GetBlog);
-router.get('/byid/:blog_id',GetBlog);
-router.post('/create', upload.single("blogImage"),CreateBlogValidationError ,CreateBlog);
-router.put('/like/:blog_id',likeBlog);
-router.put('/update/:blog_id',upload.single("blogImage"),updateBlog);
+router.get('/byid/:blog_id', GetBlog);
+router.post('/create', upload.single("blogImage"), CreateBlogValidationError, CreateBlog);
+router.put('/like/:blog_id', likeBlog);
+router.put('/update/:blog_id', upload.single("blogImage"), updateBlog);
 
-router.post('/:blog_id/comment',CreateCommentValidationError,createComment);
+router.post('/:blog_id/comment', CreateCommentValidationError, createComment);
 router.delete('/delete/:blog_id', deleteBlog);
+if (process.env.APP_ENV as string === 'testing') {
+    router.delete('/danger', deleteAllBlogs);
+}
 
 
 export { router as blogRoutes };
